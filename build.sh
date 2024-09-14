33f0d15b472d838c8ee24f32cd379beb263fb6f3
doug.holtsinger <doug.holtsinger@gmail.com>
2024-09-14 16:15:13 -0500

    Switched over to TWIM
    Switched over to TWIM
    corrected pin control for I2c

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ set -x
 #west build -p always --shield adafruit_lsm6ds3_lis3mdl -b nrf52840dongle app
 west build --pristine --shield adafruit_lsm6ds3_lis3mdl -b nrf52840dongle app
 
+# Find the SEGGER RTT block for use with the J-Link RTT Viewer
+grep _SEGGER_RTT build/zephyr/zephyr_final.map
+
